a38af4195ee31d1c7c0487d7356d49cc1e85d6d2
aaa12377 <55227836+aaa12377@users.noreply.github.com>
2020-01-09 15:28:55 +0800

    E1080308

---
+++ Unity_UI_E1080308_Tina/Assets/scripts/gamemanager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public Text loadingText;
+    public Slider loading;
+
+    public void SetVBGM(float value)
+    {
+        mixer.SetFloat("VBGM",value)
+    }
+
+    public void SetVSFX(float value)
+    {
+        mixer.SetFloat("VSFX",value)
+    }
+
+    public void Play()
+    {
+        StartCoroutine(loading());
+    }
+
+    private IEnumerator Loading()
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync("場景");
+        ao.allowSceneActivation = false;
+
+        while (ao.isDone == false)
+        {
+            loadingText.text = ((ao.progress / 0.9f) * 100).ToString();
+            loading.value = ao.progress / 0.9f;
+            yield return new WaitForSeconds(0.0001f);
+
+            if (ao.progress == 0.9f && Input.anyKey)
+            {
+                ao.allowSceneActivation = true;
+            }
+        }
+           
+              
+
+    }
+}
